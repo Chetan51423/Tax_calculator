82293d3e98c48e0c20410b85a953d918514843c6
Chetan51423 <chetanshinde51423@gamil.com>
2024-04-15 18:07:36 +0530

    add validations, add warning for gross annual income, add check if all the input fields are filled, check if the gross annual income input is only munbers

--- Index.js
+++ Index.js
@@ -46,12 +46,90 @@ function set_FinalIncome() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    if (document.getElementById("submit_button")) {
-        const submit_button = document.getElementById("submit_button");
-        submit_button.addEventListener("click", function() {
-            const ans = Final_Income();
-            window.location.href = "./index2.html?ans=" + ans;
-        });
+
+    const currentPage = window.location.pathname;
+    console.log(currentPage)
+
+    const input = document.getElementById("annual_income");
+    const circle = document.querySelector(".svg2 circle");
+    const text = document.querySelector(".svg2 text");
+    const tooltip_warning = document.getElementById("tooltip_warning");
+    
+    function isNumber()
+    {
+        const input = document.getElementById("annual_income");
+        const inputValue = input.value.trim();
+        const isNumber_ = /^\d+$/.test(inputValue);
+        if(isNumber_)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    if(currentPage=="/Index.html")
+    {
+        input.addEventListener("input", function(){
+            if(input.value.length >0)
+            {
+                // console.log(input.value);
+                // const inputValue = input.value.trim();
+                // const isNumber = /^\d+$/.test(inputValue);
+                // console.log(isNumber);
+                if (isNumber(input.value)) {
+                circle.setAttribute("stroke", "grey");
+                text.setAttribute("fill", "black");
+
+                } else {
+                    tooltip_warning.style.visibility ="visible"
+                    circle.setAttribute("stroke", "red");
+                    text.setAttribute("fill", "red");
+                    
+                }
+            }
+            else
+            {
+                tooltip_warning.style.visibility ="hidden"
+                circle.setAttribute("stroke", "grey");
+                text.setAttribute("fill", "black"); 
+            }
+        })
+    }
+    
+
+    const submitButton = document.getElementById("submit_button");
+
+    if(submitButton)
+    {
+        submitButton.addEventListener('click', function(event)
+        {
+            event.preventDefault();
+
+            const annualIncome = document.getElementById("annual_income").value.trim();          
+            const extraIncome = document.getElementById("extra_income").value.trim();       
+            const ageGroup = document.getElementById("age_group").value.trim();
+            const deduction = document.getElementById("deduction").value.trim();
+
+
+            if(!annualIncome || !extraIncome || ageGroup=="_" || !deduction)
+            {
+                alert("please fill all the fields");
+                return;
+            }
+            else if(!isNumber())
+            {
+                alert("enter the numerical value in gross income")
+                return;
+            }
+            else
+            {
+                const ans = Final_Income();
+                window.location.href = "./index2.html?ans=" + ans;
+            }
+        })
     }
 
     if (document.getElementById("close_button")) {
